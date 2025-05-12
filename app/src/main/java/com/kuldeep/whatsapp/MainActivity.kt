cbbdc74cbbefab8bb14fae4d62b0f4a13c6f7495
kuldeep <kuldeepahirwarmba@gmail.com>
2025-05-12 13:08:42 +0530

    Changes testing purpose

--- app/src/main/java/com/kuldeep/whatsapp/MainActivity.kt
+++ app/src/main/java/com/kuldeep/whatsapp/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             WhatsAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Whatsapp",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
